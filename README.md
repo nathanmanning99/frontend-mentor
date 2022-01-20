28c41d46f7c592a7f56cb9d598d6805f587718fb
nathanmanning99 <nathanmanningdev@gmail.com>
2022-01-20 23:05:24 +0000

    first commit and code base architecture set up

---
+++ README.md
@@ -0,0 +1,9 @@
+# Frontend Mentor - Instant user validaiton & sanization  
+
+Approach: Mobile first 
+
+#ES6 JS 
+#SCSS
+#HTML
+
+Built with pure css, no framework used.
---
+++ app/app.js
@@ -0,0 +1,168 @@
+const checkFirstName = (firstname) => {
+  let valid = false;
+  const fn = firstname.value.trim();
+  if (!isRequired(fn)) {
+    showError(firstname, "This field is mandatory*");
+  } else if (firstname.value.length < 2) {
+    showError(firstname, "Please check first name");
+  } else {
+    showSuccess(firstname);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkLastName = (lastname) => {
+  let valid = false;
+  const ln = lastname.value.trim();
+  if (!isRequired(ln)) {
+    showError(lastname, "This field is mandatory*");
+  } else if (lastname.value.length < 2) {
+    showError(lastname, "Please check last name");
+  } else {
+    showSuccess(lastname);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkEmail = (email) => {
+  let valid = false;
+  const ea = email.value.trim();
+  if (!isRequired(ea)) {
+    showError(email, "This field is mandatory*");
+  } else if (!isEmailValid(ea)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+    valid = true;
+  }
+  return valid;
+};
+
+const passwordChecker = (password1, password2) => {
+  let valid = false;
+
+  const p1 = password1.value.trim();
+  const p2 = password2.value.trim();
+
+  if (password1.value.length < 8) {
+    showError(password1, "Password must be 8 characters or more");
+    showError(password2, "Password must be 8 characters or more");
+  } else if (p1.search(/[a-z]/i) < 0) {
+    showError(password1, "Your password must contain at least one letter.");
+  } else if (p1.search(/[0-9]/) < 0) {
+    showError(password1, "Your password must contain at least one digit.");
+  } else if (p1 !== p2) {
+    showError(password1, "Passwords are not the same");
+    showError(password2, "Passwords are not the same");
+  } else {
+    showSuccess(password1);
+    showSuccess(password2);
+    valid = true;
+  }
+
+  return valid;
+};
+
+
+
+const isEmailValid = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+const isRequired = (value) => (value === "" ? false : true);
+
+const showError = (input, message) => {
+  const formField = input.parentElement;
+  const error = formField.querySelector("small");
+  error.textContent = message;
+  input.style.border = "1.5px solid #EB0000";
+};
+
+const showSuccess = (input) => {
+  const formField = input.parentElement;
+  const error = formField.querySelector("small");
+  error.textContent = "";
+  input.style.border = "1.5px solid hsl(154, 59%, 51%)";
+};
+
+function validateMyForm(
+  form,
+  firstname,
+  lastname,
+  email,
+  password1,
+  password2
+) {
+  checkFirstName(firstname);
+  checkLastName(lastname);
+  checkEmail(email);
+  passwordChecker(password1, password2);
+
+  let formValid =
+    checkFirstName(firstname) &&
+    checkLastName(lastname) &&
+    checkEmail(email) &&
+    passwordChecker(password1, password2);
+
+  // if form is valid then submit
+  if (formValid) {
+    form.style.display = "none";
+  }
+
+  const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+      // cancel the previous timer
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      // setup a new timer
+      timeoutId = setTimeout(() => {
+        fn.apply(null, args);
+      }, delay);
+    };
+  };
+
+  //debounce function to provide instant user feeback upon form submission
+  form.addEventListener(
+    "input",
+    debounce(function (e) {
+      switch (e.target.id) {
+        case "enq-first-name":
+          checkFirstName(firstname);
+          break;
+        case "enq-last-name":
+          checkLastName(lastname);
+          break;
+        case "enq-email":
+          checkEmail(email);
+          break;
+        case "enq-pw1":
+          passwordChecker(password1, password2);
+          break;
+        case "enq-pw2":
+          passwordChecker(password1, password2);
+          break;
+      }
+    })
+  );
+}
+
+
+
+const myArray = [
+  { name: 'chris', location: 'on city' },
+  { name: 'chris2', location: 'coon city' },
+  { name: 'chris3', location: 'ran city' },
+  { name: 'chris4', location: 'ron city' }
+];
+
+console.table(myArray)
+
+console.log(myArray)
+console.log(myArray[0])
+console.log(myArray[0].location)
